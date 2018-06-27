d931257a54999c56c6e866741c57a77db41eba97
Sagar Patel <sagaritrockz@users.noreply.github.com>
2018-06-27 15:38:08 +0200

    Create factory.js

---
+++ js/factory.js
@@ -0,0 +1,22 @@
+//factory.js
+
+	function contactInformationFactory () {
+		var id = 0;
+		return {
+			newContactInfoModel: function () {
+				return {
+					id: undefined,
+					firstName: undefined,
+					lastName: undefined,
+					email: undefined,
+					phone: undefined,
+					isActive: true
+				}
+			},			
+			generateId: function () {
+				return ++id;
+			}
+		}
+	}
+
+	contactInformationApp.factory('contactInformationFactory', contactInformationFactory);
